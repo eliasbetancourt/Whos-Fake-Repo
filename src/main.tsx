8451ced4af3dc693a6bd7023437b8dc1425ecd16
Elias Betancourt <eliaskbetancourt@gmail.com>
2025-08-25 22:32:37 -0400

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WhosFakeApp from './WhosFakeApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <WhosFakeApp />
+  </React.StrictMode>
+)
